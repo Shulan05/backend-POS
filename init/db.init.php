59e2812065a76fbf87a497955480ad0bb84bc01c
Shulan05 <somavateybrang@gmail.com>
2025-04-20 10:54:58 +0700

    last commit

--- init/db.init.php
+++ init/db.init.php
@@ -1,13 +1,16 @@
 <?php
-$host = '127.0.0.1'; //you can use localhost
-$dbname = 'webapp';
-$user = 'root';
-$password = '';
+$host = 'fdb1029.awardspace.net'; // Use the actual MySQL host provided by your hosting
+$dbname = '4618332_tey';              // Make sure this DB exists in your hosting panel
+$user = '4618332_tey';           // Your database username
+$password = 'xt;*G(nk7vJcWz.(';  // Your database password
 
-//mysql connect
+// Create a new MySQL connection
 $db = new mysqli($host, $user, $password, $dbname);
 
+// Check the connection
 if ($db->connect_error) {
-    echo 'Connection failed.' . $db->connect_error;
-    die();
+    die('Connection failed: ' . $db->connect_error);
 }
+
+
+?>
